6d613da587ca1b9b11134cfd41591f693c75e755
Claire Bain <claire.bain3@gmail.com>
2022-10-20 18:29:08 -0700

    worked on javascript and html

--- script.js
+++ script.js
@@ -2,9 +2,16 @@
 var timerEl = document.getElementById('countdown');
 var headerEl = document.getElementById('header');
 var subheaderEl = document.getElementById('subheader');
+var listEl = document.getElementById('listanswers');
+var li1 = document.createElement("li");
+var li2 = document.createElement("li");
+var li3 = document.createElement("li");
+var li4 = document.createElement("li");
+var startQuiz = document.getElementById('startQuiz');
 
 var codingQuestions = ['Question 1', 'Question 2', 'Question 3', 'Question 4', 'Question 5']
 // how to store the codingAnswers? Is an array best?
+var codingAnswer1 = ['Answer 1', 'Answer 2', 'Answer 3', 'Answer 4', 'Answer 5']
 
 var timeLeft
 var userScore
@@ -15,7 +22,8 @@ function gameOver(){
     subheaderEl.textContent = 'You final score is ' + userScore
 
 }
-
+headerEl.textContent = 'Coding Quiz'
+subheaderEl.textContent = 'Start Quiz'
 
 // Timer that counts down from 5
 function countdown() {
@@ -45,9 +53,38 @@ function countdown() {
   }
 
   function dispayQuestions(){
-    var x = 0
-    headerEl.textContent = codingQuestions[x]
-
+    var  x = x +1;
+    var currentQuestion = codingQuestions[x];
+    headerEl.textContent = currentQuestion
+    subheaderEl.textContent
+    li1.textContent = codingAnswer1[x]
+    listEl.appendChild(li1);
+listEl.appendChild(li2);
+listEl.appendChild(li3);
+listEl.appendChild(li4);
   }
 
-  countdown()
+  var li1 = document.createElement("li");
+var li2 = document.createElement("li");
+var li3 = document.createElement("li");
+var li4 = document.createElement("li");
+
+//   function navigate(direction) {
+//     index = index + direction;
+//     if (index < 0) { 
+//       index = images.length - 1; 
+//     } else if (index > images.length - 1) { 
+//       index = 0;
+//     }
+//     currentImage = images[index];
+//     carousel.style.backgroundImage = "url('" + currentImage + "')";
+//   }
+
+
+  startQuiz.addEventListener("click", function(event) {
+    // Stops event from bubbling up and new window opening
+    event.stopPropagation();
+    countdown()
+    
+  });
+
