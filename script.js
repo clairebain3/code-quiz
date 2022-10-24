05d55513baf466074f253a7e3178906ab2aaaa9b
Claire Bain <claire.bain3@gmail.com>
2022-10-23 20:50:36 -0700

    working on javascript functions

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 var timerEl = document.getElementById('countdown');
 var headerEl = document.getElementById('header');
 var subheaderEl = document.getElementById('subheader');
-var listEl = document.getElementById('listanswers');
+var listEl = document.getElementById('listAnswers');
 var li1 = document.createElement("li");
 var li2 = document.createElement("li");
 var li3 = document.createElement("li");
@@ -11,11 +11,16 @@ var startQuiz = document.getElementById('startQuiz');
 
 var codingQuestions = ['Question 1', 'Question 2', 'Question 3', 'Question 4', 'Question 5']
 // how to store the codingAnswers? Is an array best?
-var codingAnswer1 = ['Answer 1', 'Answer 2', 'Answer 3', 'Answer 4', 'Answer 5']
+var codingAnswer0 = ['Answer 1', 'Answer 2', 'Answer 3', 'Answer 4']
+var codingAnswer1 = ['Answer 1', 'Answer 2', 'Answer 3', 'Answer 4']
+var codingAnswer2 = ['Answer 1', 'Answer 2', 'Answer 3', 'Answer 4']
+var codingAnswer3 = ['Answer 1', 'Answer 2', 'Answer 3', 'Answer 4']
+var codingAnswer4 = ['Answer 1', 'Answer 2', 'Answer 3', 'Answer 4']
 
 var timeLeft
 var userScore
 var userIntials
+var questionCount = 0
 
 function gameOver(){
     headerEl.textContent = 'All Done'
@@ -23,9 +28,9 @@ function gameOver(){
 
 }
 headerEl.textContent = 'Coding Quiz'
-subheaderEl.textContent = 'Start Quiz'
+subheaderEl.textContent = 'Click the button to start the quiz. Answering incorrectly will subtract 5 seconds from the timer'
 
-// Timer that counts down from 5
+// Timer that counts down from 60
 function countdown() {
     var timeLeft = 60;
   
@@ -46,28 +51,50 @@ function countdown() {
         timerEl.textContent = '';
         // Use `clearInterval()` to stop the timer
         clearInterval(timeInterval);
-        // Call the `displayMessage()` function
+        // Call the `gameOver()` function
        gameOver();
       }
     }, 1000);
   }
 
   function dispayQuestions(){
-    var  x = x +1;
-    var currentQuestion = codingQuestions[x];
-    headerEl.textContent = currentQuestion
-    subheaderEl.textContent
-    li1.textContent = codingAnswer1[x]
+    // this will just display the first question and remove the start button
+    // remove the start quiz button
+    startQuiz.setAttribute("style", "display:none;");
+    listEl.textContent = codingQuestions[0];
+    // display the first question and answer set
+    li1.textContent = codingAnswer0[0];
+    li2.textContent = codingAnswer0[1];
+    li3.textContent = codingAnswer0[2];
+    li4.textContent = codingAnswer0[3];
     listEl.appendChild(li1);
-listEl.appendChild(li2);
-listEl.appendChild(li3);
-listEl.appendChild(li4);
+    listEl.appendChild(li2);
+    listEl.appendChild(li3);
+    listEl.appendChild(li4);
+
   }
 
-  var li1 = document.createElement("li");
-var li2 = document.createElement("li");
-var li3 = document.createElement("li");
-var li4 = document.createElement("li");
+  function nextQuestion(){
+    if(questionCount < 5){
+    questionCount = questionCount + 1
+    listEl.textContent = codingQuestions[questionCount];
+    li1.textContent = eval('codingAnswer' + questionCount);
+  }
+    else{
+      gameOver();
+
+    }
+  }
+
+  answers.on("click", ".letter-button", function (event) {
+    var displayLetterEl = $("<div>");
+  
+    displayLetterEl.addClass("letter");
+  
+    // get letter from clicked letter button's `data-letter` attribute and use it for display
+    displayLetterEl.text($(event.target).attr("data-letter"));
+    displayEl.append(displayLetterEl);
+  });
 
 //   function navigate(direction) {
 //     index = index + direction;
@@ -84,7 +111,7 @@ var li4 = document.createElement("li");
   startQuiz.addEventListener("click", function(event) {
     // Stops event from bubbling up and new window opening
     event.stopPropagation();
-    countdown()
-    
+    countdown();
+    dispayQuestions();
   });
 
